01ad12a2712fe05e6f804749281cdffe5f08ca93
Rishabh Gupta <rishotics@gmail.com>
2024-01-09 17:25:43 +0530

    add generator and verif key

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
-use ark_ec::AffineRepr;
+use ark_ec::{AffineRepr, CurveConfig};
 use ark_ec::short_weierstrass::Affine;
+use ark_ff::{MontFp, QuadExtConfig};
 use ark_std::UniformRand;
 use plonky2::field::extension::Extendable;
 // use plonky2::field::types::Field;
@@ -13,17 +14,36 @@ use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig};
 use plonky2::{
     field::{goldilocks_field::GoldilocksField, types::PrimeField64},
 };
+use plonky2_ecdsa::curve::curve_types::Curve;
+use rayon::str;
 use core::num::ParseIntError;
 use std::char::from_digit;
 // use pairing::G1Point;
 
-use ark_bn254::{Fq12, G1Affine, G2Affine, Config};
+use ark_bn254::{Fq12, G1Affine, G2Affine, Config, Fq};
+use ark_bn254::{Fq2, Fr, G2Projective};
+// use ark_bls12_381::{Fq, Fq2, Fr, Fq12, G1Affine, G2Affine, G1Projective, G2Projective};
 use ark_ff::fields::Field;
+
 use itertools::Itertools;
 
 use plonky2_bn254_pairing::pairing::pairing;
 
-use ark_bn254::{Fq2, Fr, G2Projective};
+
+pub struct VerificationKey {
+    pub alpha1: G1Affine,
+    pub beta2: G2Affine,
+    pub gamma2: G2Affine,
+    pub delta2: G2Affine,
+    pub ic: Vec<G1Affine>,
+
+}
+
+pub struct Proof {
+    pub a: Vec<G1Affine>,
+    pub b: Vec<G2Affine>,
+    pub c: Vec<G1Affine>
+}
 
 
 #[allow(non_snake_case)]
@@ -51,15 +71,82 @@ fn main() {
     // let sipp_proof = sipp_prove_native(&A, &B);
     let aa = A[0];
     let ax = aa.x();
-    println!("A[0] = {:?}", A[0]);
-    println!("B[0] = {:?}", B[0]);
+    // println!("A[0] = {:?}", A[0]);
+    // println!("B[0] = {:?}", B[0]);
     // let sipp_statement = sipp_verify_native(&A, &B, &sipp_proof).unwrap();
-    let prod = inner_product(&A, &B);
-    println!("prod = {:?}", prod);
+    // let prod = inner_product(&A, &B);
+    // println!("prod = {:?}", prod);
 
-    let new_pt = Affine::<F>::from({
-        
-    })
+    let g1 = G1Affine::new(Fq::from(1), Fq::from(2));
+    println!("g1 point loaded");
+    let g2 = 
+        G2Affine::new(
+            Fq2::new(
+                MontFp!("10857046999023057135944570762232829481370756359578518086990519993285655852781"),
+
+                MontFp!("11559732032986387107991004021392285783925812861821192530917403151452391805634"),
+            ),
+            Fq2::new(
+                MontFp!("8495653923123431417604973247489272438418190587263600148770280649306958101930"),
+                MontFp!("4082367875863433681332203403145435568316851327593401208105741076214120093531"),
+
+            )
+        );
+
+
+
+    println!("g1 = {:?}", g1);
+    println!("g2 = {:?}", g2);
+
+
+    let mut vk = VerificationKey {
+        alpha1: G1Affine::new(
+            Fq::from(1),
+            Fq::from(2),
+        ),
+        beta2: G2Affine::new(
+            Fq2::new(
+                MontFp!("10857046999023057135944570762232829481370756359578518086990519993285655852781"),
+
+                MontFp!("11559732032986387107991004021392285783925812861821192530917403151452391805634"),
+            ),
+            Fq2::new(
+                MontFp!("8495653923123431417604973247489272438418190587263600148770280649306958101930"),
+                MontFp!("4082367875863433681332203403145435568316851327593401208105741076214120093531"),
+
+            )
+        ),
+        gamma2: G2Affine::new(
+            Fq2::new(
+                MontFp!("10857046999023057135944570762232829481370756359578518086990519993285655852781"),
+
+                MontFp!("11559732032986387107991004021392285783925812861821192530917403151452391805634"),
+            ),
+            Fq2::new(
+                MontFp!("8495653923123431417604973247489272438418190587263600148770280649306958101930"),
+                MontFp!("4082367875863433681332203403145435568316851327593401208105741076214120093531"),
+
+            )
+        ),
+        delta2: G2Affine::new(
+            Fq2::new(
+                MontFp!("10857046999023057135944570762232829481370756359578518086990519993285655852781"),
+
+                MontFp!("11559732032986387107991004021392285783925812861821192530917403151452391805634"),
+            ),
+            Fq2::new(
+                MontFp!("8495653923123431417604973247489272438418190587263600148770280649306958101930"),
+                MontFp!("4082367875863433681332203403145435568316851327593401208105741076214120093531"),
+
+            )
+        ),
+        ic: vec![G1Affine::new(
+            Fq::from(1),
+            Fq::from(2),
+        )],
+    };
+
+    
 
     // G1Affine::from(Affine;
     // verify_proof()
